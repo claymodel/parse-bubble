f4d08d5c8f52be660205ced51d73a75ea2034220
claymodel <android.hasnat@gmail.com>
2013-09-04 14:13:35 +0900

    mBaaS PARSE feature implementation

---
+++ obj/local/armeabi/objs/modplug-1.0/load_wav.o.d
@@ -0,0 +1,73 @@
+obj/local/armeabi/objs/modplug-1.0/load_wav.o: jni/load_wav.cpp \
+ jni/libmodplug/stdafx.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h \
+ /usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdio.h \
+ jni/libmodplug/sndfile.h
+
+jni/libmodplug/stdafx.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/usr/local/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+jni/libmodplug/sndfile.h:
